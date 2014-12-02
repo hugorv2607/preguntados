134e90dff5e10abbe6727de6a51023f2b4c4ac4c
villcahugo <villcahugo@gmail.com>
2014-12-02 17:59:09 -0300

    First commit

---
+++ tp-preguntados/src/main/java/unlam/tp_preguntados/App.java
@@ -0,0 +1,13 @@
+package unlam.tp_preguntados;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
